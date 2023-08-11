b6291a02e5a1135c7e8a7e6a1d89bd278903f7d0
Sahil <ss6611786@gmail.com>
2023-08-11 23:13:58 +0530

    so state works same in react-native

--- components/ComTwo.js
+++ components/ComTwo.js
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native'
+import React, { useState } from 'react'
 
 const ComTwo = ({ name }) => {
+    const [isStart, setIsStart] = useState(true);
     const val = 'take more deep';
     const getFullName = (
         name, shyam, coll
@@ -11,6 +12,11 @@ const ComTwo = ({ name }) => {
     return (
         <View>
             <Text>Hello,{getFullName(name, 'deepak', 'jai bol')},{val}</Text>
+            <Button
+                onPress={() => { setIsStart(!isStart) }}
+                disabled={!isStart}
+                title={isStart ? 'started' : 'click to start'}
+            />
         </View>
     )
 }
